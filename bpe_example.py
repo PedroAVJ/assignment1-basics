93a412118aa05b24252120f2f3ec37d78f7fb978
Pedro Antonio Villanueva Juarez <pedroantoniovillanuevajuarez@gmail.com>
2026-07-29 16:38:35 -0500

    Refactor BPE example: streamline pair counting and word reconstruction logic

--- bpe_example.py
+++ bpe_example.py
@@ -18,32 +18,28 @@ for _ in range(6):
     for word, frequency in frequency_table.items():
         for left, right in pairwise(word):
             pair_counts[(left, right)] += frequency
-    items = list(pair_counts.items())
-    new: list[tuple[int, tuple[bytes, bytes]]] = []
-    for payload, frequency in items:
-        new.append((frequency, payload))
-    _, most_frequent_pair = max(new)
+    most_frequent_pair, _ = max(pair_counts.items(), key=lambda kv: (kv[1], kv[0]))
     vocabulary[len(vocabulary)] = b"".join(most_frequent_pair)
 
-    new: dict[tuple[bytes, ...], int] = {}
     for word_bytes, frequency in list(frequency_table.items()):
-        skip = False
-        new_word: list[bytes] = []
-        cur = 0
-        length = len(word_bytes)
-        while cur in range(length):
-            next = cur + 1
-            if next not in range(length):
-                new_word.append(word_bytes[cur])
+        for pair in pairwise(word_bytes):
+            if most_frequent_pair == pair:
+                frequency_table.pop(word_bytes)
+                new_word: list[bytes] = []
+                cur = 0
+                length = len(word_bytes)
+                while cur in range(length):
+                    next = cur + 1
+                    if next not in range(length):
+                        new_word.append(word_bytes[cur])
+                        break
+                    if most_frequent_pair == (word_bytes[cur], word_bytes[next]):
+                        new_word.append(b"".join(most_frequent_pair))
+                        cur += 1
+                    else:
+                        new_word.append(word_bytes[cur])
+                    cur += 1
+                frequency_table[tuple(new_word)] = frequency
                 break
-            if most_frequent_pair == (word_bytes[cur], word_bytes[next]):
-                new_word.append(b"".join(most_frequent_pair))
-                cur += 1
-            else:
-                new_word.append(word_bytes[cur])
-            cur += 1
-        new[tuple(new_word)] = frequency
-
-    frequency_table = new
 
 vocabulary[len(vocabulary)] = b"<|endoftext|>"
